35e25ab9fff2a9124d6eea28353e6206371dd386
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-07-24 23:02:59 -0400

    Fixed bugs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,18 @@
 const app = require('express').Router();
-const botsdata = require("../database/models/botlist/bots.js")
+const botsdata = require("../database/models/botlist/bots.js");
 
-console.log("[GhostyLists]: Index router loaded.")
+console.log("[vcodes.xyz]: Index router loaded.");
 
 app.get("/", async (req, res) => {
     res.render("index.ejs", {
         bot: global.Client,
         path: req.path,
         config: global.config,
-        user: rq.isAuthenticated() ? req.user : null,
+        user: req.isAuthenticated() ? req.user : null,
         req: req,
         botdata: await botsdata.find(),
         roles: global.config.server.roles,
-        channels: global.config.server.chanels
+        channels: global.config.server.channels
     })
 })
 
